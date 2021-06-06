7f49aca19709260817b700351c1d9d9baa628694
Oleksandr Balitskiy <popingalov@gmail.com>
2021-06-06 22:26:33 +0400

    3

---
+++ src/js/scroll.js
@@ -0,0 +1,15 @@
+$(function() {
+   $('.scrollup').click(function() {
+        $("html, body").animate({
+      scrollTop:0
+    },700);
+  })
+})
+$(window).scroll(function() {
+  if ($(this).scrollTop()>200) {
+        $('.scrollup').fadeIn();
+  }
+   else {
+    $('.scrollup').fadeOut();
+  }
+});
