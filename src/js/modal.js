bd921d6be2e3b6623b9d034afea34a0aa54c2f24
Oleksandr Balitskiy <popingalov@gmail.com>
2021-06-10 00:28:28 +0400

    3

--- src/js/modal.js
+++ src/js/modal.js
@@ -12,3 +12,18 @@
     refs.modal.classList.toggle('is-hidden');
   }
 })();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-openn]'),
+    closeModalBtn: document.querySelector('[data-modal-closee]'),
+    modal: document.querySelector('[data-modall]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
