32f7413435b69c3af5cfb2872a919035895afd36
shershun <shershunserg@gmail.com>
2021-06-11 00:10:20 +0300

    branch

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -2,7 +2,7 @@ $(function () {
   $('.scrollup').click(function () {
     $("html, body").animate({
       scrollTop: 0
-    }, 750);
+    }, 500);
   })
 })
 $(window).scroll(function () {
@@ -12,4 +12,14 @@ $(window).scroll(function () {
   else {
     $('.scrollup').fadeOut();
   }
+});
+
+
+
+var $page = $('html, body');
+$('a[href*="#"]').click(function () {
+  $page.animate({
+    scrollTop: $($.attr(this, 'href')).offset().top
+  }, 400);
+  return false;
 });
